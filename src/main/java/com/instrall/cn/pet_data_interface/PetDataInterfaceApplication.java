b2a0a6777a04defbfcc09a15711efc9bc3f89065
LiuMingKun <571039838@qq.com>
2019-11-06 17:58:57 +0800

    首次提交

---
+++ src/main/java/com/instrall/cn/pet_data_interface/PetDataInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.instrall.cn.pet_data_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDataInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetDataInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/instrall/cn/pet_data_interface/PetDataInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instrall.cn.pet_data_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDataInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
